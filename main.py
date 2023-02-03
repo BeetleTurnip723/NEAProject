c3b2bbcb2477e2787cdae599e326884c99a5e26c
Joe Sykes <joesykes@Joes-MacBook-Air.local>
2023-02-03 10:20:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+'''
+NEA Year 12 Project
+Author: Joe Sykes
+Date Started: 9/9/22
+Idea:
+My idea for my NEA is to make a formula one set-up simulator where you get randomised a track, and either if it is raining or not, and you choose your driver and try to create the perfect set-up for them to win the race
+'''
+# automake the other drivers set-ups and then give a recommendation to the reader to say what they can do better
+import PyQt5
+import pygame
+
