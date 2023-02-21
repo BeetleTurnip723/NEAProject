d7348424617af9c737f898268aad44acf4fe238b
joesykes <joesykes09@gmail.com>
2023-02-21 12:01:19 +0000

    data structure

--- main.py
+++ main.py
@@ -9,4 +9,52 @@ My idea for my NEA is to make a formula one set-up simulator where you get rando
 import PyQt5
 import pygame
 
-ahdshiudsphufsihufsihus
+# for cornering, F is for fast cornering, M is for medium cornering, S is for slow cornering
+
+drivers = {
+    "Mercedes": {
+        "Lewis Hamilton": {
+            "95",               # OVR
+            "96",               # Pace
+            "96",               # Carefulness
+            "92",               # Wet-weather Driving
+            "F"                 # Cornering Style
+        },
+        "George Russell": {
+            "89",               # OVR
+            "93",               # Pace
+            "84",               # Carefulness
+            "90",               # Wet-weather Driving
+            "M"                 # Cornering Style
+        }
+    },
+
+    "Red Bull": {
+        "Max Verstappen": {
+            "97",               # OVR
+            "99",               # Pace
+            "92",               # Carefulness
+            "99",               # Wet-weather Driving
+            "F"                 # Cornering Style
+        },
+        "Sergio Perez": {
+            "88",               # OVR
+            "82",               # Pace
+            "95",               # Carefulness
+            "87",               # Wet-weather Driving
+            "S"                 # Cornering Style
+        }
+    },
+
+    "Ferrari": {
+        "Charles Leclerc": {
+            "91",               # OVR
+            "94",               # Pace
+            "90",               # Carefulness
+            "88",               # Wet-weather Driving
+            "M"                 # Cornering Style
+        }
+    },
+
+
+}
